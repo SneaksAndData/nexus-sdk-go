8479ff1f0eceac317defaecc6fe1d4e46835496f
George Zubrienko <gzu@ecco.com>
2025-07-17 17:20:28 +0200

    WIP

--- sdk/scheduler_test.go
+++ sdk/scheduler_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/SneaksAndData/nexus-core/pkg/telemetry"
 	api "github.com/SneaksAndData/nexus-sdk-go/pkg/generated/scheduler"
+	"github.com/go-faster/jx"
 	"k8s.io/klog/v2"
 	"strings"
 	"testing"
@@ -44,52 +45,72 @@ func Test_GetRunResultsTagDoesNotExist(t *testing.T) {
 		}
 	}
 }
-
-func Test_GetRunResultsTagExists(t *testing.T) {
+func Test_PostNonExistingAlgorithm(t *testing.T) {
 	f := newFixture(t)
-	expectedLength := 3
-	actualLength := 0
-	for _, err := range f.client.GetRunResults("abc", nil) {
-		if err != nil {
-			f.t.Error(err)
-		}
-		actualLength++
+	request := &api.ModelsAlgorithmRequest{
+		AlgorithmParameters: api.ModelsAlgorithmRequestAlgorithmParameters{
+			"algorithm": jx.Raw("smth"),
+			"settingA":  jx.Raw("a"),
+			"settingB":  jx.Raw("b"),
+		},
 	}
 
-	if actualLength != expectedLength {
-		f.t.Errorf("GetRunResults should have returned the expected %d submissions", expectedLength)
+	_, err := f.client.CreateRun(request, "non-existing")
+	if err == nil {
+		f.t.Error("CreateRun should have returned an error, since algorithm 'non-existing' is not deployed")
+	}
+
+	if err != nil && !strings.Contains(err.Error(), "no submissions found for tag") {
+		f.t.Error("Incorrect error returned, should be: no submissions found for tag")
 	}
 }
 
-func Test_AwaitRun(t *testing.T) {
-	f := newFixture(t)
-	runId, err := f.client.CreateRun(&api.ModelsAlgorithmRequest{
-		AlgorithmParameters: nil,
-		CustomConfiguration: api.OptV1NexusAlgorithmSpec{
-			Set: false,
-		},
-		ParentRequest: api.OptModelsAlgorithmRequestRef{
-			Set: false,
-		},
-		PayloadValidFor: api.OptString{
-			Set: false,
-		},
-		RequestApiVersion: api.OptString{
-			Set: false,
-		},
-		Tag: api.OptString{
-			Value: "abc",
-			Set:   true,
-		},
-	}, "omni-channel-solver")
-
-	if err != nil {
-		f.t.Error(err)
-	}
-
-	_, err = f.client.AwaitRun(runId, "omni-channel-solver", nil)
-
-	if err != nil {
-		f.t.Error(err)
-	}
-}
+//
+//func Test_GetRunResultsTagExists(t *testing.T) {
+//	f := newFixture(t)
+//	expectedLength := 3
+//	actualLength := 0
+//	for _, err := range f.client.GetRunResults("abc", nil) {
+//		if err != nil {
+//			f.t.Error(err)
+//		}
+//		actualLength++
+//	}
+//
+//	if actualLength != expectedLength {
+//		f.t.Errorf("GetRunResults should have returned the expected %d submissions", expectedLength)
+//	}
+//}
+//
+//func Test_AwaitRun(t *testing.T) {
+//	f := newFixture(t)
+//	runId, err := f.client.CreateRun(&api.ModelsAlgorithmRequest{
+//		AlgorithmParameters: nil,
+//		CustomConfiguration: api.OptV1NexusAlgorithmSpec{
+//			Set: false,
+//		},
+//		ParentRequest: api.OptModelsAlgorithmRequestRef{
+//			Set: false,
+//		},
+//		PayloadValidFor: api.OptString{
+//			Set: false,
+//		},
+//		RequestApiVersion: api.OptString{
+//			Set: false,
+//		},
+//		Tag: api.OptString{
+//			Value: "abc",
+//			Set:   true,
+//		},
+//	}, "omni-channel-solver")
+//
+//	if err != nil {
+//		f.t.Error(err)
+//	}
+//
+//	_, err = f.client.AwaitRun(runId, "omni-channel-solver", nil)
+//
+//	if err != nil {
+//		f.t.Error(err)
+//	}
+//}
